41fe69496255b4563b905813394011771169a000
lej9310 <lej9310@gmail.com>
2025-10-29 11:53:17 +0900

    10/29-mappersql수정

--- src/main/java/com/lej/ex25_branch/mapper/StudentMapper.java
+++ src/main/java/com/lej/ex25_branch/mapper/StudentMapper.java
@@ -2,42 +2,37 @@ package com.lej.ex25_branch.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.lej.ex25_branch.domain.Student;
 
 @Mapper
 public interface StudentMapper {
 
-	// 전체 조회 =========================
-	// 1. 조회
+	// 1. 전체 조회
 	List<Student> findAll();
 
-	// 단건 조회 =========================
+	// StudentMapper.xml의 쿼리 이동 ======================================
 	// 2. 등록(삽입)
+	@Select("SELECT * FROM student WHERE id = #{id}")
 	Student findById(Long id);
 
+	// 3. 삽입
+	@Insert("INSERT INTO student(NAME, email, age) \r\n" + "  	VALUES (#{name}, #{email}, #{age})")
+	@Options(useGeneratedKeys = true, keyProperty = "id")
 	void insert(Student student);
 
-	// SELECT * FROM student WHERE id = #{id}
-	// Student findById(Long id);
+	// 4. 수정
+	@Update("UPDATE student\r\n" + "	SET NAME = #{name}, email = #{email}, age = #{age}\r\n" + "	WHERE id = #{id}")
+	void update(Student student);
 
-	// @Insert("INSERT INTO student(NAME, email, age) VALUES\r\n" + " (#{name},
-	// #{email}, #{age})")
-	// @Options(useGeneratedKeys = true, keyProperty = "id")
-	// void insert(Student student);
-
-	// 3.수정
-	void update(Student student);	
-
-	// @Update("UPDATE student\r\n"
-	// + "SET NAME = #{name}, email = #{email}, age = #{age}\r\n"
-	// + "WHERE id = #{id}")	
-	// void update(Student student);
-	
-	// 4. 삭제
+	// 5. 삭제
+	@Delete("DELETE FROM student WHERE id = #{id}")
 	void delete(Long id);
-	
-	// @Delete("DELETE FROM student WHERE id = #{id}")
-	// void delete(Long id);
+
 }
