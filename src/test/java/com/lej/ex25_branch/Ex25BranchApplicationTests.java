679a65d2dc7b2ab4300be2d2d9ea6dac7f667dcc
lej9310 <lej9310@gmail.com>
2025-10-29 14:36:41 +0900

    10/29-tdd
    Test-Driven Development

--- src/test/java/com/lej/ex25_branch/Ex25BranchApplicationTests.java
+++ src/test/java/com/lej/ex25_branch/Ex25BranchApplicationTests.java
@@ -3,6 +3,9 @@ package com.lej.ex25_branch;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// TDD(Test-Driven Development): 코드 작성 전에 테스트 작성 - Red >> Green >> Refactor
+
+
 @SpringBootTest
 class Ex25BranchApplicationTests {
 
---
+++ src/test/java/com/lej/ex25_branch/mapper/StudentMapperTest.java
@@ -0,0 +1,37 @@
+package com.lej.ex25_branch.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+import com.lej.ex25_branch.domain.StudentTest;
+
+@Mapper
+public class StudentMapperTest {
+
+	@Select("SELECT * FROM student ORDER BY #{id} DESC")
+	List<StudentTest> findAll();
+
+	@Select("SELECT * FROM student WHERE id = #{id}")
+	StudentTest findById(Long id);
+
+	// @Insert("INSERT INTO student(NAME, email, age) \r\n"
+	// + " VALUES (#{name}, #{email}, #{age})")
+	// @Options(useGeneratedKeys = true, keyProperty = "id")
+	// void insert(Student student);
+
+	// @Update("UPDATE student\r\n"
+	// + " SET NAME = #{name}, email = #{email}, age = #{age}\r\n"
+	// + " WHERE id = #{id}")
+	// void update(Student student);
+
+	// @Delete("DELETE FROM student WHERE id = #{id}")
+	// void delete(Long id);
+
+	@Insert("INSERT INTO student(NAME, email, age) \r\n" + "  	VALUES (#{name}, #{email}, #{age})")
+	@Options(useGeneratedKeys = true, keyProperty = "id")
+	int insert(StudentTest student);
+}
